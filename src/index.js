0181d42c6094007ec2d0d6a2665d9c7732ce6beb
Lennin666 <soporte@allnexus.com.co>
2021-02-17 08:54:33 -0500

    agregando configuracion inicial

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/estilos.scss'
+
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('app'));
