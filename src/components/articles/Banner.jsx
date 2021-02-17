66f0cc4fc7e4df5925df5dafcccbc72fd8d57e7a
Lennin666 <soporte@allnexus.com.co>
2021-02-17 17:35:58 -0500

    implementando el banner para las noticias

---
+++ src/components/articles/Banner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Banner = () => (
+  <>
+    <div className='banner' />
+    <div><p className='tittle'>noticia</p></div>
+  </>
+)
+
+export default Banner
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Banner from '../components/articles/Banner';
 
-const Home = () => <h1>Hola lennin</h1>;
+const Home = () => (
+  <Banner />
+);
 
 export default Home;
