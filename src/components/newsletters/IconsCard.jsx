896037a0f0489f07d66f0601fedac0e436f29055
Lennin666 <soporte@allnexus.com.co>
2021-02-17 21:08:36 -0500

    implementando un sistema de grillas

--- src/components/newsletters/IconsCard.jsx
+++ src/components/newsletters/IconsCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const IconsCard = () => (
-  <div>
+  <div className='iconosCard'>
     <i className="far fa-star"> 50 </i>
     <i className="far fa-comment-alt">50</i>
     <i className="far fa-eye">75</i>
