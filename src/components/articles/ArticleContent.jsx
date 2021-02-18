2a5d2e6b9d1de996457ca15f4ee88138f778cbc0
Lennin666 <soporte@allnexus.com.co>
2021-02-18 11:18:40 -0500

    creando contenedores para la sección de articulos

---
+++ src/components/articles/ArticleContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from './Banner';
+import ContentArticle from './ContentArticle';
+import TittleArticle from './TittleArticle';
+import InfoArticles from './InfoArticles';
+import Tags from './Tags';
+
+const ArticleContent = () => (
+  <>
+    <Banner />
+    <TittleArticle />
+    <InfoArticles />
+    <Tags />
+    <ContentArticle />
+  </>
+);
+
+export default ArticleContent;
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
-import Banner from '../components/articles/Banner';
-import ContentArticle from '../components/articles/ContentArticle';
-import TittleArticle from '../components/articles/TittleArticle';
-import News from '../components/newsletters/News';
-import Tags from '../components/articles/Tags';
-import InfoArticles from '../components/articles/InfoArticles';
+import ArticleContent from '../components/articles/ArticleContent';
+import Newslettercontens from '../components/newsletters/Newlettercontens';
 
 const Home = () => (
   <section className="home">
     <section className="home__article">
-      <Banner />
-      <TittleArticle />
-      <InfoArticles />
-      <Tags />
-      <ContentArticle />
+      <ArticleContent />
     </section>
 
     <section className="home__new">
-      <div className="home__tittle">
-        <h3>Noticias relacionadas</h3>
-        <hr />
-      </div>
-      <News />
-      <News />
-      <News />
-      <News />
+      <Newslettercontens />
     </section>
   </section>
 );
